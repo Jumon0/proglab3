41fd5c15dccff3cb02bc957716e6c3bcc882fa76
Anatoliy <tolik.kan2005@mail.ru>
2024-12-23 09:11:45 +0300

    Update Leafy.java

--- lab3/src/Alive/Leafy.java
+++ lab3/src/Alive/Leafy.java
@@ -7,10 +7,6 @@ public class Leafy extends Shorties{
         super("Листик",18, Places.CIRCUS);
     }
 
-    public Leafy(String name, int age, Places place) {
-        super(name, age, place);
-    }
-
     @Override
     public String toString() {
         return "Коротышка по имени: " + getName() + ", возрастом " + getAge() + ", находится в " + getPlace();
